b12cd57e076300710c87931c53589d4a851dcbbf
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-27 17:36:04 -0500

    Se agrego las aplicaciones

--- Aplicaciones/Propiedades/apps.py
+++ Aplicaciones/Propiedades/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PropiedadesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Propiedades'
+    name = 'Aplicaciones.Propiedades'
