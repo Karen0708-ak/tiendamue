75a95df538b8aed2ae6783ed196f4a31f5439b91
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-07-19 21:25:03 -0500

    Se agrego la eliminacion ddesde administrador

--- Aplicaciones/Administrador/urls.py
+++ Aplicaciones/Administrador/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admiin', views.admiin, name='admiin'),
+    path('eliminar_propiedad/<id_propiedad>', views.eliminar_propiedad, name='eliminar_propiedad'),
     path('usuarios', views.usuarios, name='usuarios'),
 ]
--- Aplicaciones/Administrador/views.py
+++ Aplicaciones/Administrador/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render,redirect
-
+from Aplicaciones.Publicaciones.models import Propiedad
 from Aplicaciones.Usuario.models import Usuario
 # Create your views here.
 def admiin(request):
     if 'admin_id' not in request.session:
         return redirect('iniciosesion')
-    return render(request, 'admiin.html')
+    propiedades = Propiedad.objects.select_related('usuario').all()
+    return render(request, 'admiin.html', {'propiedades': propiedades})
 
 ##############LISTA DE USUARIOS############
 def usuarios(request):
     usuarios = Usuario.objects.all()
     return render(request, 'usuarios.html', {'usuarios': usuarios})
+
+############Eliminar publicacion por denuncia###########
+from django.shortcuts import get_object_or_404, redirect
+from Aplicaciones.Notificacion.models import Notificacion
+from django.contrib import messages
+
+def eliminar_propiedad(request, id_propiedad):
+    if 'admin_id' not in request.session:
+        messages.error(request, 'Debes iniciar sesión como administrador.')
+        return redirect('iniciosesion')
+
+    propiedad = get_object_or_404(Propiedad, pk=id_propiedad)
+    usuario = propiedad.usuario  
+
+
+    mensaje = f'Tu publicación "{propiedad.titulo}" fue eliminada por el administrador debido a denuncias.'
+    Notificacion.objects.create(
+        usuario=usuario,
+        mensaje=mensaje,
+    )
+
+    propiedad.delete()
+    messages.success(request, 'Propiedad eliminada y usuario notificado correctamente.')
+    return redirect('admiin')
